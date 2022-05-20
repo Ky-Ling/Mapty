9063a42b506a5e7ef01c951f7c53dc3bf4bb1a9b
GC <2845740149@qq.com>
2022-05-20 20:35:37 +0800

    Display a Map using Leaflet Library

--- script.js
+++ script.js
@@ -11,18 +11,32 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+if (navigator.geolocation)
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      // console.log(position);
 
-if(navigator.geolocation) 
-    navigator.geolocation.getCurrentPosition(function(position) {
-        // console.log(position);
+      const { latitude } = position.coords;
+      // const latitude = position.coords.altitude;
+      const longitude = position.coords.longitude;
+      console.log(latitude, longitude);
+      console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-        const {latitude} = position.coords;
-        // const latitude = position.coords.altitude;
-        const longitude = position.coords.longitude;
-        console.log(latitude, longitude);
-        console.log(`https://www.google.com/maps/@${latitude},${longitude}`);
 
-    }, function() {
-        alert("Could not get your position.");
-    });
+      const coords = [latitude, longitude];
+      const map = L.map('map').setView(coords, 13);
 
+      L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.fr/hot/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('Could not get your position.');
+    }
+  );
